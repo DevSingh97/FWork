c0f619bf706405bda4f20aa2160d7a608b60669b
Knight_Fury97 <69894850+DevSingh97@users.noreply.github.com>
2021-01-12 19:23:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const fullnav = document.querySelector('#fullnav');
+const close = document.querySelector('#fclose');
+
+menu.addEventListener('click', function(){
+    fullnav.style.right ='0';
+});
+
+close.addEventListener('click',function(){
+    fullnav.style.right = '-100%';
+});
